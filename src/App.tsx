9f45160ec4a86dc15b867d9e4fbdfc75c462aedb
Rita Rixter <ritarixter@mail.ru>
2022-01-24 20:16:45 +0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import styles from './App.module.css';
+import AppHeader from './components/AppHeader/AppHeader';
+import BurgerIngredients from './components/BurgerIngredients/BurgerIngredients'
+import BurgerConstructor from './components/BurgerConstructor/BurgerConstructor';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppHeader/>
+      <main className={styles.app}>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </main>
+    </>
   );
 }
 
