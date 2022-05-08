761320e31991f1c3f56fee40d560551ccb04f753
Margo <ritarixter@mail.ru>
2022-05-08 17:21:35 +0300

    Доделала функционал dnd

--- src/services/actions/order.jsx
+++ src/services/actions/order.jsx
@@ -1,5 +1,4 @@
 import { dataOrder } from "../../utils/API";
-
 export const SET_ORDER_NUMBER = "SET_ORDER_NUMBER";
 export const SET_ORDER_NUMBER_FAILED = "SET_ORDER_NUMBER_FAILED";
 export const SET_OPEN = "SET_OPEN";
